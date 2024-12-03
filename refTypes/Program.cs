8c552781ab0d458e325dd0b418f00de9f3ea16ac
Volodymyr Solovei <solovey.itstep@gmail.com>
2024-12-03 11:51:27 +0200

    Add project files.

---
+++ refTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿using refTypes;
+
+var act = new Actions();
+var name = act.CreateToy();
+
+Console.WriteLine(name);
+Console.ReadKey();
+
+
+
+
---
+++ refTypes/Toy.cs
@@ -0,0 +1,24 @@
+﻿namespace refTypes;
+internal class Toy
+{
+    public string Name { get; set; } = string.Empty;
+}
+
+internal class Actions
+{
+    public string CreateToy()
+    {
+        Toy toy = new Toy();
+        toy.Name = "some toy";
+        Play(ref toy);
+
+        return toy.Name;
+    }
+
+    public void Play(ref Toy toy)
+    {
+        toy = new Toy();
+
+        toy.Name = "not real name";
+    }
+}
