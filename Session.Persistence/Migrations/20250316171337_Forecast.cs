57032cf7625e72e8fb5c3b7c1270466dce07d8d0
Volodymyr Solovei <solovey.itstep@gmail.com>
2025-05-22 08:23:29 +0300

    unit tests project

--- Session.Persistence/Migrations/20250316171337_Forecast.cs
+++ Session.Persistence/Migrations/20250316171337_Forecast.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace SessionMVC.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class Forecast : Migration
     {
         /// <inheritdoc />
